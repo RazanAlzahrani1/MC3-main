9c3aa5efb1e63f33d4eada9881743dd48a5571a6
Razan Alzahrani <1razanalzahrani1@gmail.com>
2024-01-30 15:56:57 +0300

    Initial Commit

---
+++ MC3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MC3
+//
+//  Created by Arwa Alosaimi on 12/07/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MC3/MC3App.swift
@@ -0,0 +1,17 @@
+//
+//  MC3App.swift
+//  MC3
+//
+//  Created by Arwa Alosaimi on 12/07/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct MC3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
